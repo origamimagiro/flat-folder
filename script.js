13f380832e9b05977e2b0e4630eb03fd94556319
Jason Ku <jasonku@mit.edu>
2022-07-22 23:06:16 +0800

    generating constraints, removed constraints.js, Closes #1

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-import {CONSTRAINTS, CON, NOC} from "./constraints.js";
-
 window.onload = () => { MAIN.startup(); }   // entry point
 
 const MAIN = {
     startup: () => {
+        NOTE.clear_log();
+        NOTE.start("*** Starting Flat-Folder ***");
+        NOTE.time("Initializing interface");
         const [b, s] = [50, SVG.SCALE];
         const main = document.getElementById("main");
         for (const [k, v] of Object.entries({
@@ -40,6 +41,9 @@ const MAIN = {
                 file_reader.readAsText(e.target.files[0]);
             }
         };
+        NOTE.time("Computing constraint implication maps");
+        CON.build();
+        NOTE.end();
     },
     process_file: (e) => {
         NOTE.clear_log();
@@ -237,7 +241,7 @@ const SOLVER = {    // STATE SOLVER
             if (a == undefined) { debugger; }
             return ((x < y) || (a == 0)) ? a : ((a == 1) ? 2 : 1);
         });
-        const I = CONSTRAINTS[NOC[type]][tuple.join("")];
+        const I = CON.implied[type].get(tuple.join(""));
         if (I == undefined) { debugger; }
         if (I == 0) { return false; }   // assignment not possible
         if (I == 1) { return [];    }   // possible, but nothing inferable
@@ -1934,3 +1938,76 @@ const NOTE = {  // ANNOTATION
     },
 };
 
+const CON = {      // CONSTRAINTS
+    taco_taco: 0,
+    taco_tortilla: 1,
+    tortilla_tortilla: 2,
+    transitivity: 3,
+    valid: [
+        ["111112", "111121", "111222", "112111",    // 0: taco-taco
+         "121112", "121222", "122111", "122212", 
+         "211121", "211222", "212111", "212221", 
+         "221222", "222111", "222212", "222221"],
+        ["112", "121", "212", "221"],               // 1: taco-tortilla
+        ["11", "22"],                               // 2: tortilla-tortilla
+        ["112", "121", "122", "211", "212", "221"], // 3: transitivity
+    ],
+    implied: [],
+    build: () => {
+        for (const type of [0, 1, 2, 3]) {
+            const n = CON.valid[type][0].length;
+            const I = [];
+            for (let i = 0; i <= n; ++i) {
+                I.push(new Map());
+            }
+            for (let i = 0; i < 3**n; ++i) {
+                let [k, num_zeros] = [i, 0];
+                const A = [];
+                for (let j = 0; j < n; ++j) {
+                    const val = k % 3;
+                    num_zeros += (val == 0) ? 1 : 0;
+                    A.push(val);
+                    k = (k - A[j]) / 3;
+                }
+                I[num_zeros].set(A.join(""), 0);
+            }
+            for (const k of CON.valid[type]) {
+                I[0].set(k, 1);
+            }
+            for (let i = 1; i <= n; ++i) {
+                for (const [k, _] of I[i]) {
+                    const A = Array.from(k);
+                    let good = 0;
+                    for (let j = 0; j < n; ++j) {
+                        const check = [];
+                        if (A[j] == "0") {
+                            for (const c of ["1", "2"]) {
+                                A[j] = c;
+                                if (I[i - 1].get(A.join("")) != 0) {
+                                    check.push([j, +c]);
+                                }
+                            }
+                            A[j] = "0";
+                            if ((good == 0) && (check.length > 0)) {
+                                good = [];
+                            }
+                            if (check.length == 1) {
+                                good.push(check[0]);
+                            }
+                        }
+                    }
+                    if (Array.isArray(good) && (good.length == 0)) {
+                        good = 1;
+                    }
+                    I[i].set(k, good);
+                }
+            }
+            CON.implied[type] = new Map();
+            for (let i = n; i >= 0; --i) {
+                for (const [k, v] of I[i]) {
+                    CON.implied[type].set(k, v);
+                }
+            }
+        }
+    },
+};
