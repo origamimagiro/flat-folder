76ae93ebdb4a5653057570991f9e3644420ad402
Mu-Tsun Tsai <don.m.t.tsai@gmail.com>
2023-04-26 17:57:57 +0800

    Throttle worker check call

--- src/worker/note.js
+++ src/worker/note.js
@@ -3,8 +3,18 @@
 export const NOTE = new Proxy({}, {
     get(target, name) {
         target[name] = target[name] || function(...args) {
+            if(name == "start_check") {
+                last_check = performance.now();
+            }
+            if(name == "check") {
+                const now = performance.now();
+                if(now - last_check < 5000) return;
+                else last_check = now;
+            }
             postMessage({ name, args });
         };
         return target[name];
     }
 });
+
+let last_check = performance.now();
