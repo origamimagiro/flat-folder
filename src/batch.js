075db6576bdf1f25be21304787c566b90a7bdfae
Jason Ku <jasonku@me.com>
2024-07-08 20:17:59 +0800

    fixed batch processing with new api

--- src/batch.js
+++ src/batch.js
@@ -12,11 +12,14 @@ if("window" in globalThis) {
     window.onload = () => BATCH.main();   // entry point
 }
 
-let wn;
+let W;
 export const BATCH = {
     main: async () => {
-        wn = ((window.Worker == undefined) ? 1
+        const wn = ((window.Worker == undefined) ? 1
             : (navigator.hardwareConcurrency ?? 8));
+        if ((wn != undefined) && (wn > 1)) {
+            W = await PAR.get_workers(wn, "./src/worker.js");
+        }
         NOTE.time("Computing constraint implication maps");
         CON.build();
         const limit_select = document.getElementById("limit_select");
@@ -96,24 +99,31 @@ export const BATCH = {
         const [,CP] = X.V_EV_2_VV_FV(P, SP);
         const [SC, CS] = X.EV_FV_2_EF_FE(SP, CP);
         const [CF, FC] = X.EF_FV_SP_SE_CP_SC_2_CF_FC(EF, FV, SP, SE, CP, SC);
-        const ExE = X.SE_2_ExE(SE);
-        const ExF = X.SE_CF_SC_2_ExF(SE, CF, SC);
         const BF = X.EF_SP_SE_CP_CF_2_BF(EF, SP, SE, CP, CF);
         const BI = new Map();
-        for (const [i, F] of BF.entries()) { BI.set(F, i); }
-        const [BT0, BT1, BT2] = X.BF_BI_EF_ExE_ExF_2_BT0_BT1_BT2(
-            BF, BI, EF, ExE, ExF);
-        let BT3;
-        if ((wn != undefined) && (wn > 1)) {
-            const W = await PAR.get_workers(wn, "./src/worker.js");
-            BT3 = await X.FC_CF_BF_W_2_BT3(FC, CF, BF, W);
-            await PAR.end_workers(W);
-        } else {
-            BT3 = X.EF_SP_SE_CP_FC_CF_BF_2_BT3(EF, SP, SE, CP, FC, CF, BF);
+        const num = {};
+        let BT;
+        {
+            for (const [i, F] of BF.entries()) { BI.set(F, i); }
+            const ExE = X.SE_2_ExE(SE);
+            const ExF = X.SE_CF_SC_2_ExF(SE, CF, SC);
+            const [BT0, BT1, BT2] = X.BF_BI_EF_ExE_ExF_2_BT0_BT1_BT2(
+                BF, BI, EF, ExE, ExF);
+            num.BT0 = NOTE.count_subarrays(BT0)/6;
+            num.BT1 = NOTE.count_subarrays(BT1)/2;
+            num.BT2 = NOTE.count_subarrays(BT2)/2;
+            const BT3x = X.FC_BF_BI_BT0_BT1_2_BT3x(FC, BF, BI, BT0, BT1);
+            const ex_trans = NOTE.count_subarrays(BT3x)/3;
+            let BT3;
+            if (W != undefined) {
+                BT3 = await X.FC_CF_BF_BT3x_W_2_BT3(FC, CF, BF, BT3x, W);
+            } else {
+                BT3 = X.EF_SP_SE_CP_FC_CF_BF_2_BT3(EF, SP, SE, CP, FC, CF, BF);
+            }
+            num.red_trans = NOTE.count_subarrays(BT3)/3;
+            num.tot_trans = ex_trans + num.red_trans;
+            BT = BF.map((F,i) => [BT0[i], BT1[i], BT2[i], BT3[i]]);
         }
-        const init_trans = NOTE.count_subarrays(BT3)/3;
-        X.FC_BF_BI_BT0_BT1_BT3_2_clean_BT3(FC, BF, BI, BT0, BT1, BT3);
-        const BT = BF.map((F,i) => [BT0[i], BT1[i], BT2[i], BT3[i]]);
         const out = SOLVER.initial_assignment(EF, EA, Ff, BF, BT, BI);
         if ((out.length == 3) && (out[0].length == undefined)) {
             const [type, F, E] = out;
@@ -141,11 +151,11 @@ export const BATCH = {
             faces: FV.length,
             eps: eps,
             variables: BF.length,
-            "taco-taco": NOTE.count_subarrays(BT0)/6,
-            "taco-tortilla": NOTE.count_subarrays(BT1)/2,
-            "tortilla-tortilla": NOTE.count_subarrays(BT2)/2,
-            transitivity: init_trans,
-            reduced_trans: NOTE.count_subarrays(BT3)/3,
+            "taco-taco": num.BT0,
+            "taco-tortilla": num.BT1,
+            "tortilla-tortilla": num.BT2,
+            transitivity: num.tot_trans,
+            reduced_trans: num.red_trans,
             components: GB.length,
             limited: (GA.reduce((s, A) => (s && (A.length != lim)), true)
                 ? "no limit" : lim),
